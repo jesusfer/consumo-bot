3fe0fb4ab795f644f09a7033a8f1679151f502b2
Jesus Fernandez <jesus@nublar.net>
2020-02-28 12:28:33 +0100

    Added formatting configs

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 // MIT License
 
 module.exports = {
-    token: process.env.TELEGRAM_BOT_TOKEN,
-    azureAccount: process.env.AZURE_STORAGE_ACCOUNT,
-    azureKey: process.env.AZURE_STORAGE_KEY,
-    azureTable: process.env.AZURE_TABLE_NAME,
-    allowedUsers: process.env.TELEGRAM_ALLOWED_USERS.split(',').map(t => parseInt(t)),
+  token: process.env.TELEGRAM_BOT_TOKEN,
+  azureAccount: process.env.AZURE_STORAGE_ACCOUNT,
+  azureKey: process.env.AZURE_STORAGE_KEY,
+  azureTable: process.env.AZURE_TABLE_NAME,
+  allowedUsers: process.env.TELEGRAM_ALLOWED_USERS.split(",").map((t) => parseInt(t)),
 };
--- src/start.js
+++ src/start.js
@@ -1,20 +1,19 @@
 // Copyright (c) 2018 Jesús Fernández <jesus@nublar.net>
 // MIT License
 
-'use strict';
+"use strict";
 
-const bot = require('./bot');
+const bot = require("./bot");
 
-console.log('Bot started: ' + (new Date()).toUTCString());
+console.log("Bot started: " + new Date().toUTCString());
 
 function handler() {
-    console.log('Bot exiting: ' + (new Date()).toUTCString());
-    bot.stopPolling()
-        .then(function () {
-            process.exit();
-        });
+  console.log("Bot exiting: " + new Date().toUTCString());
+  bot.stopPolling().then(function() {
+    process.exit();
+  });
 }
 
-process.on('SIGINT', handler);
-process.on('SIGTERM', handler);
-process.on('exit', handler);
+process.on("SIGINT", handler);
+process.on("SIGTERM", handler);
+process.on("exit", handler);
